ab3c3b3b1def178b12b38d105a5e931025f213dc
Samy-Garcia <20240528@ricaldone.edu.sv>
2026-08-31 11:14:32 -0600

    init

---
+++ src/config/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+import { API_KEY, AUTH_DOMAIN, PROJECT_ID, STORAGE_BUCKET, MESSAGING_SENDER_ID, APP_ID } from "@env";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID
+};
+
+console.log("Valor de congiguración de Firebase:", firebaseConfig);
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const database = getFirestore(app);
+
+const storage = getStorage(app);
+
+export { database, storage };
