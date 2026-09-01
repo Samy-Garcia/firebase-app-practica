592c3484d368bbd6439659b6a95255a9a95bd4e0
Samy-Garcia <20240528@ricaldone.edu.sv>
2026-09-01 14:20:06 -0600

    errores arreglados

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,16 @@
 module.exports = function (api) {
   api.cache(true);
+
   return {
-    presets: ['babel-preset-expo'],
-    plugin: [
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          unstable_transformProfile: 'hermes-v0',
+        },
+      ],
+    ],
+    plugins: [
       [
         'module:react-native-dotenv',
         {
@@ -12,4 +20,4 @@ module.exports = function (api) {
       ],
     ],
   };
-}
+};
